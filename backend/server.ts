11d30c2907b6d05707eb7a1a8669716959678343
Mohsin3725 <133794289+Mohsin3725@users.noreply.github.com>
2026-06-25 00:11:02 +0500

    daily update

--- backend/server.ts
+++ backend/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import authRoutes from './modules/auth/auth.routes';
+import authRoutes from './modules/auth/auth.routes.ts';
 import adminRoutes from './modules/admin/admin.routes';
 
 dotenv.config();
